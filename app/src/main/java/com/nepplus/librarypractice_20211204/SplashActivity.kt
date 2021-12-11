9e0b3c93e8d9be97d8622f4735699305f010ad5e
BradlyKim <kjh5864@naver.com>
2021-12-11 11:45:01 +0900

    메인화면으로 넘어가는 화면 구현

--- app/src/main/java/com/nepplus/librarypractice_20211204/SplashActivity.kt
+++ app/src/main/java/com/nepplus/librarypractice_20211204/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.nepplus.librarypractice_20211204
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +15,19 @@ class SplashActivity : AppCompatActivity() {
     }
 
     fun  setupEvents(){}
-    fun setValues(){}
+    fun setValues(){
+
+//        2초가 지나면 => 메인화면으로 이동.
+
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed( {
+
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+
+            finish()
+
+        }, 2000 )
+
+    }
 }
